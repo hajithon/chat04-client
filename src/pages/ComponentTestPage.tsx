7b78ba6831c3e2c67778d774267e151e9c2ba583
Mungjin01 <beckaljm@naver.com>
2024-07-27 22:08:27 +0900

    chore: svgr 오류 해결

---
+++ src/pages/ComponentTestPage.tsx
@@ -0,0 +1,9 @@
+import { ReactComponent as Gray } from '@/assets/logos/gray.svg';
+export const ComponentTestPage = () => {
+  return (
+    <>
+      <Gray />
+      <div>팀원들과 릴레이로 어쩌구</div>
+    </>
+  );
+};
--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import { ChatRoomCreatePage } from '@/pages/ChatRoomCreatePage';
 import { ChatRoomJoinPage } from '@/pages/ChatRoomJoinPage';
 import { ChatRoomPage } from '@/pages/ChatRoomPage';
+import { ComponentTestPage } from '@/pages/ComponentTestPage';
 import { ParticipantsJoinPage } from '@/pages/ParticipantsJoinPage';
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/page1" element={<ChatRoomCreatePage />} />
+      <Route path="/component" element={<ComponentTestPage />} />
       <Route path="/:roomId" element={<ChatRoomPage />} />
       <Route path="/ChatRoomJoin" element={<ChatRoomJoinPage />} />
       <Route path="/ParticipantsJoin/:roomId" element={<ParticipantsJoinPage />} />
